a1c15217b97a07dd1ad7d796e311ef2bfba11e06
Carl <Carl.heintz02@gmail.com>
2024-10-07 10:13:56 +0200

    feat: MAJ Api, ActorCard, Movie Crad, MovieDetails

--- wr506d-mmi22/src/components/MovieCard.vue
+++ wr506d-mmi22/src/components/MovieCard.vue
@@ -1,32 +1,43 @@
 <script setup>
 import axios from 'axios'
 import { onMounted, ref } from 'vue'
-import ActorCard from './ActorCard.vue';
+import { useRouter } from 'vue-router'
+import ActorCard from './ActorCard.vue'
 
-let recup =ref([]);
-let selectedActor = ref(null);
+let recup = ref([])
+let selectedActor = ref(null)
+const router = useRouter()
 
 onMounted(async () => {
   try {
-    const response = await axios.get('http://symfony.mmi-troyes.fr:8319/api/movies');
-    recup.value = response.data["hydra:member"];
+    const response = await axios.get('http://symfony.mmi-troyes.fr:8319/api/movies')
+    console.log(response.data)
+    recup.value = response.data['hydra:member']
   } catch (error) {
-    console.error("Erreur lors de la récupération des films : ", error);
+    console.error('Erreur lors de la récupération des films : ', error)
   }
-});
+})
 
-
+const goToDetails = (movieId) => {
+  router.push(`/movies/${movieId}`)
+}
 </script>
 
 <template>
   <div id="lit">
     <h1>Les derniers films</h1>
     <div v-if="recup.length > 0" class="movies-container">
-        <div v-for="movie in recup.slice(-4)" :key="movie.id" class="movie-card">
+      "
+      <div
+        v-for="movie in recup.slice(-4)"
+        :key="movie.id"
+        class="movie-card"
+        @click="goToDetails(movie.id)"
+      >
         <h2>{{ movie.title }}</h2>
         <img v-if="movie.media" :src="movie.media" alt="Affiche du film" />
         <p>{{ movie.description }}</p>
-        </div>
+      </div>
     </div>
 
     <p v-else>Aucun film à afficher</p>
@@ -47,23 +58,23 @@ h1 {
 
 .movies-container {
   display: flex;
-  justify-content: space-between; /* Espacement égal entre les films */
-  gap: 20px; /* Espacement entre les cartes de films */
-  flex-wrap: wrap; /* Pour s'assurer que si l'écran est plus petit, les éléments vont à la ligne */
+  justify-content: space-between;
+  gap: 20px;
+  flex-wrap: wrap;
 }
 
 .movie-card {
   flex: 1;
-  max-width: 22%; /* Chaque carte occupe environ 22% de la largeur pour 4 films */
-  background-color: #f4f4f4; /* Fond légèrement gris pour différencier chaque carte */
+  max-width: 22%;
+  background-color: #f4f4f4;
   padding: 10px;
-  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1); /* Ajout d'une ombre pour la profondeur */
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
   border-radius: 8px;
-  text-align: center; /* Centrer les éléments à l'intérieur */
+  text-align: center;
   transition: transform 0.3s;
 }
 .movie-card:hover {
-  transform: scale(1.05); 
+  transform: scale(1.05);
   transition: transform 0.3s;
 }
 
@@ -75,7 +86,7 @@ h2 {
 img {
   max-width: 100%;
   height: auto;
-  border-radius: 5px; /* Bords arrondis pour l'image */
+  border-radius: 5px;
   margin-bottom: 10px;
 }
 
