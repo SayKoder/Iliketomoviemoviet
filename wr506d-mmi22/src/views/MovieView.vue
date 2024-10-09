ab7e491400ee9df01ccb2a991435ffef53da5649
Carl <Carl.heintz02@gmail.com>
2024-10-09 15:28:50 +0200

    feat: adding data to movie page + searchbar

--- wr506d-mmi22/src/views/MovieView.vue
+++ wr506d-mmi22/src/views/MovieView.vue
@@ -1,16 +1,100 @@
-<script lang="js" setup>
-const message = "Bienvenue sur la page des films";
+<script lang="ts" setup>
+import axios from 'axios'
+import { onMounted, ref, computed } from 'vue'
+import MovieCard from '../components/MovieCard.vue'
+
+const message = 'Bienvenue sur la page des films'
+const searchQuery = ref('')
+
+const recup = ref([])
+
+const filteredMovies = computed(() => {
+  return movies.filter((movie) =>
+    movie.title.toLowerCase().includes(searchQuery.value.toLowerCase())
+  )
+})
 </script>
 
 <template>
   <div>
     <h1>{{ message }}</h1>
-    <p>Liste des films à venir...</p>
+    <input
+      type="text"
+      v-model="searchQuery"
+      placeholder="Rechercher un film..."
+      class="search-bar"
+    />
+    <p>Liste des films :</p>
+    <MovieCard>
+      <template v-slot:default="{ movies }">
+        <div class="movies-container">
+          <ul v-if="movies.length > 0" class="movies-container">
+            <li v-for="movie in movies" :key="movie.id" class="movie-card">
+              <h3>{{ movie.title }}</h3>
+              <img v-if="movie.media" :src="movie.media" alt="Affiche du film" />
+              <p>{{ movie.description }}</p>
+            </li>
+          </ul>
+        </div>
+      </template>
+    </MovieCard>
   </div>
 </template>
 
 <style scoped>
 h1 {
   color: #42b983;
+}
+
+.search-bar {
+  width: 100%;
+  padding: 10px;
+  font-size: 16px;
+  margin-bottom: 20px;
+  border: 1px solid #ccc;
+  border-radius: 5px;
+}
+
+.movies-container {
+  display: flex;
+  justify-content: space-between;
+  gap: 20px;
+  flex-wrap: wrap;
+}
+
+.movie-card {
+  display: flex;
+  justify-content: space-between;
+  flex-wrap: wrap;
+  max-width: 22%;
+  background-color: #f4f4f4;
+  padding: 10px;
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+  border-radius: 8px;
+  text-align: center;
+  transition: transform 0.3s;
+}
+.movie-card:hover {
+  transform: scale(1.05);
+  transition: transform 0.3s;
+}
+
+ul {
+  display: flex;
+  list-style: none;
+  padding: 0;
+}
+
+li {
+  margin: 5px 0;
+  padding: 5px;
+  color: black;
+}
+
+img {
+  max-width: 100%;
+  height: auto;
+  border-radius: 5px;
+  margin-bottom: 10px;
 }
 </style>
