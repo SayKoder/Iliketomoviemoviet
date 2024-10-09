9bdbe5bb9a66b2e89e92b60712aae691263c3a28
Carl <Carl.heintz02@gmail.com>
2024-10-09 14:58:47 +0200

    feat: new add data on moviecard and actorcard

--- wr506d-mmi22/src/views/ActorDetails.vue
+++ wr506d-mmi22/src/views/ActorDetails.vue
@@ -1,12 +1,22 @@
-<script setup>
+<script lang="ts" setup>
 import { ref, onMounted } from 'vue'
 import { useRoute } from 'vue-router'
 import axios from 'axios'
-import MovieCard from '../components/MovieCard.vue'
 
 const route = useRoute()
 const actorId = ref(route.params.id)
 const actorDetails = ref(null)
+const movies = ref([])
+
+const formatDate = (dateString: string, format: string = 'iso') => {
+  const date = new Date(dateString)
+
+  if (format === 'long') {
+    return date.toLocaleDateString('fr-FR', { year: 'numeric', month: 'long', day: 'numeric' }) // Ex: 3 juillet 2023
+  } else {
+    return date.toLocaleDateString('fr-CA') // Ex: 2023-07-03 (format ISO court)
+  }
+}
 
 let selectedMovie = ref(null)
 const message = 'Bienvenue sur la page des détails des acteurs'
@@ -16,7 +26,13 @@ onMounted(async () => {
     const response = await axios.get(
       `http://symfony.mmi-troyes.fr:8319/api/actors/${actorId.value}`
     )
-    actorDetails.value = response.data //Stockage des détails du film
+    actorDetails.value = response.data //Stockage des détails acteurs
+    const movieDetails = actorDetails.value.movies.map(async (movieUrl: string) => {
+      const movieResponse = await axios.get(`http://symfony.mmi-troyes.fr:8319${movieUrl}`)
+      return movieResponse.data
+    })
+
+    movies.value = await Promise.all(movieDetails) //Stockage des détails des films
   } catch (error) {
     console.error('Erreur lors de la récupération des détails des acteurs : ', error)
   }
@@ -34,18 +50,18 @@ onMounted(async () => {
         <p>{{ actorDetails.bio }}</p>
       </div>
       <div class="right-column">
-        <p><strong>Date de naissance :</strong> {{ actorDetails.dob }}</p>
+        <p><strong>Date de naissance :</strong> {{ formatDate(actorDetails.dob, 'long') }}</p>
         <p><strong>Nationalité :</strong> {{ actorDetails.nationality }}</p>
         <p><strong>Genre :</strong> {{ actorDetails.gender }}</p>
         <p><strong>Récompenses :</strong> {{ actorDetails.awards }}</p>
         <p><strong>Films :</strong></p>
-        <ul>
-          <li v-for="movie in actorDetails.movies" :key="movie.id">
-            <h2>{{ movie.title }}</h2>
-            <img v-if="movie.media" :src="movie.media" alt="Affiche du film" />
-            <p>{{ movie.description }}</p>
+        <ul v-if="movies.length > 0">
+          <li v-for="movie in movies" :key="movie.id">
+            <h3 class="titre-movie">{{ movie.title }}</h3>
+            <img class="img-movie" v-if="movie.media" :src="movie.media" alt="Affiche du film" />
           </li>
         </ul>
+        <p v-else>Dans aucun film</p>
       </div>
     </div>
     <p v-else>Chargement des détails...</p>
@@ -119,14 +135,26 @@ img {
 }
 
 ul {
+  display: flex;
   list-style: none;
   padding: 0;
 }
 
 li {
-  background-color: #ddd;
   margin: 5px 0;
-  padding: 10px;
+  padding: 5px;
+  color: black;
+}
+
+.img-movie {
+  max-width: 100%;
+  height: auto;
   border-radius: 5px;
+  margin-bottom: 10px;
+}
+
+.titre-movie {
+  font-size: 20px;
+  color: #4b2109;
 }
 </style>
--- wr506d-mmi22/src/views/MovieDetails.vue
+++ wr506d-mmi22/src/views/MovieDetails.vue
@@ -1,22 +1,39 @@
-<script setup>
+<script lang="ts" setup>
 import { ref, onMounted } from 'vue'
 import { useRoute } from 'vue-router'
 import axios from 'axios'
-import ActorCard from '../components/ActorCard.vue'
 
 const route = useRoute()
 const movieId = ref(route.params.id)
 const movieDetails = ref(null)
+const actors = ref([])
 
 let selectedActor = ref(null)
 const message = 'Bienvenue sur la page des détails des films'
+
+const formatDate = (dateString: string, format: string = 'iso') => {
+  const date = new Date(dateString)
+
+  if (format === 'long') {
+    return date.toLocaleDateString('fr-FR', { year: 'numeric', month: 'long', day: 'numeric' }) // Ex: 3 juillet 2023
+  } else {
+    return date.toLocaleDateString('fr-CA') // Ex: 2023-07-03 (format ISO court)
+  }
+}
 
 onMounted(async () => {
   try {
     const response = await axios.get(
       `http://symfony.mmi-troyes.fr:8319/api/movies/${movieId.value}`
     )
+    console.log(response.data)
     movieDetails.value = response.data //Stockage des détails du film
+    const actorDetails = movieDetails.value.actors.map(async (actorUrl: string) => {
+      const actorResponse = await axios.get(`http://symfony.mmi-troyes.fr:8319${actorUrl}`)
+      return actorResponse.data
+    })
+
+    actors.value = await Promise.all(actorDetails) //Stockage des détails des acteurs
   } catch (error) {
     console.error('Erreur lors de la récupération des détails du film : ', error)
   }
@@ -28,10 +45,28 @@ onMounted(async () => {
     <button @click="$router.go(-1)">Retour</button>
     <h1>{{ message }}</h1>
     <div class="detailMovie" v-if="movieDetails">
-      <h2>{{ movieDetails.title }}</h2>
-      <img v-if="movieDetails.media" :src="movieDetails.media" alt="Affiche du film" />
-      <p>{{ movieDetails.description }}</p>
-      <ActorCard />
+      <div class="left-column">
+        <h2>{{ movieDetails.title }}</h2>
+        <img v-if="movieDetails.media" :src="movieDetails.media" alt="Affiche du film" />
+        <p>{{ movieDetails.description }}</p>
+        <p><strong>Date de sortie :</strong> {{ formatDate(movieDetails.releaseDate, 'long') }}</p>
+        <p><strong>Genre :</strong> {{ movieDetails.genre }}</p>
+      </div>
+      <div class="right-column">
+        <p><strong>Acteurs du film :</strong></p>
+        <ul v-if="actors.length > 0">
+          <li v-for="actor in actors" :key="actor.id">
+            <h3 class="titre-actor">{{ actor.firstname }} {{ actor.lastname }}</h3>
+            <img
+              class="img-actor"
+              v-if="actor.media"
+              :src="actor.media"
+              alt="Affiche de l'acteur"
+            />
+          </li>
+        </ul>
+        <p v-else>Aucun acteur</p>
+      </div>
     </div>
     <p v-else>Chargement des détails...</p>
   </div>
@@ -64,14 +99,16 @@ button:hover {
 }
 
 .detailMovie {
-  flex: 1;
-  max-width: 22%;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+  width: 70%; /* 70% de la largeur de la page */
   background-color: #f4f4f4;
-  padding: 10px;
+  padding: 20px;
   box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
   border-radius: 8px;
-  text-align: center;
   transition: transform 0.3s;
+  margin: auto;
 }
 
 .detailMovie:hover {
@@ -79,10 +116,48 @@ button:hover {
   transition: transform 0.3s;
 }
 
+.left-column,
+.right-column {
+  flex: 1;
+  padding: 20px;
+}
+
+.left-column {
+  max-width: 45%; /* Colonne gauche prend environ 45% de la div */
+}
+
+.right-column {
+  max-width: 45%; /* Colonne droite prend environ 45% de la div */
+}
+
 img {
   max-width: 100%;
   height: auto;
   border-radius: 5px;
   margin-bottom: 10px;
+}
+
+ul {
+  display: flex;
+  list-style: none;
+  padding: 0;
+}
+
+li {
+  margin: 5px 0;
+  padding: 5px;
+  color: black;
+}
+
+.img-actor {
+  max-width: 100%;
+  height: auto;
+  border-radius: 5px;
+  margin-bottom: 10px;
+}
+
+.titre-actor {
+  font-size: 20px;
+  color: #4b2109;
 }
 </style>
